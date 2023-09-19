ce6622ad7a592a1bcdafd6fbae419d2122611556
Sanford Huynh <huynhsanford@gmail.com>
2023-09-19 00:54:14 +0000

    Added the beforeunload event listener to the window object, serialize the data and stores the JSON in localstorage

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 /* exported data */
 
+window.addEventListener('beforeunload', (event) => {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+});
+
 let data = {
   view: 'entry-form',
   entries: [],
